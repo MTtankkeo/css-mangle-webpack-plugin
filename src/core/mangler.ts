7dd8c03f8c118b05f85ea291d4f8c7acca3572c6
Dev Ttangkong <ttankkeo112@gmail.com>
2024-08-18 21:14:01 +0900

    Develop about a mangler

---
+++ src/core/mangler.ts
@@ -0,0 +1,35 @@
+
+export class Mangler {
+    private static _instance: Mangler;
+    private constructor() {}
+
+    count: number = 0;
+    chars = "abcdefghijklmnopqrstuvwxyz";
+    cache = new Map();
+
+    /** Gets a unique instance of the [Mangler] class. */
+    static get instance() {
+        return this._instance ?? (this._instance = new Mangler());
+    }
+
+    createName(count = this.count++) {
+        let result = "";
+        let length = this.chars.length;
+
+        while (count >= 0) {
+            result = this.chars[count % length] + result;
+
+            if (count > 0) {
+                count = Math.floor(count / length) - 1;
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    transform(from: string) {
+        return this.cache[from] ?? (this.cache[from] = this.createName());
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+import { Mangler } from "./core/mangler";
+
+console.log(Mangler.instance.transform("background"));
+console.log(Mangler.instance.transform("rearground"));
+console.log(Mangler.instance.transform("foreground"));
+console.log(Mangler.instance.transform("background"));
