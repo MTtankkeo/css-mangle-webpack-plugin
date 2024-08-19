a2c848fb54945c966746c10ffdb50ded5f7adb31
Dev Ttangkong <ttankkeo112@gmail.com>
2024-08-19 11:21:12 +0900

    Update about webpack plugin

--- src/webpack/webpack_plugin.ts
+++ src/webpack/webpack_plugin.ts
@@ -1,9 +1,11 @@
 import { Compiler } from "webpack";
 
 export class CSSMangleWebpackPlugin {
+    constructor() {}
+
     apply(compiler: Compiler) {
         compiler.hooks.done.tap("CSSMangleWebpackPlugin", (states) => {
-            
+            console.log("Hello, World!");
         });
     }
 }
--- test/webpack.config.cjs
+++ test/webpack.config.cjs
@@ -1,9 +1,10 @@
-require("css-mangle-webpack-plugin");
+const CSSMangleWebpackPlugin = require("css-mangle-webpack-plugin").default;
 
 /** @type {import("webpack").Configuration} */
 const config = {
     mode: "production",
     entry: "./src/index.js",
+    plugins: [new CSSMangleWebpackPlugin()]
 }
 
 module.exports = config;
