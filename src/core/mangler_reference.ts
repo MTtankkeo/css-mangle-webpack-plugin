877fc7ddb271d6eb4e01731a44d19b425bee1839
Dev Ttangkong <ttankkeo112@gmail.com>
2024-08-19 16:19:20 +0900

    Bug-Fix: About CSSVariableManglerReference

--- src/core/mangler_reference.ts
+++ src/core/mangler_reference.ts
@@ -21,7 +21,7 @@ export class CSSVariableDefinition extends ManglerDefinition {
         // - "--background"
         // - "--foreground"
         //
-        const regexps = syntexText.matchAll(/(?<=var\()--\w+(?=\))|((?<="|')--\w+(?="|'))/g);
+        const regexps = syntexText.matchAll(/(?<=var\()--[\w-]+(?=\))|((?<="|')--[\w-]+(?="|'))/g);
         let replacedLength = 0;
 
         for (const regexp of regexps) {
