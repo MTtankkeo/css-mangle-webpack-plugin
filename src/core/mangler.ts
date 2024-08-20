a753d3ee7f0d3d56debf1fe0673492cb01986725
Dev Ttangkong <ttankkeo112@gmail.com>
2024-08-20 10:12:12 +0900

    Added the comments about Mangler

--- src/core/mangler.ts
+++ src/core/mangler.ts
@@ -1,21 +1,35 @@
 
-export class ManglerObject {
-    originName: string;
+/**
+ * Signature for the interface that is an object with original and
+ * transformed names and a reference count.
+ */
+export interface ManglerObject {
+    originalName: string;
     identifierName: string;
     referenceCount: number;
 }
 
 export class Mangler {
+    /** This static value that is defining chars of about a base64. */
+    static chars = "abcdefghijklmnopqrstuvwxyz";
+
+    /**
+     * This value that is defining a value that increases each when
+     * a unique name is generated.
+     */
     count: number = 0;
-    chars = "abcdefghijklmnopqrstuvwxyz";
     cache = new Map<string, ManglerObject>();
 
+    /**
+     * Generates a unique name using a base-26 system based on
+     * a given a unique count number.
+     */
     createName(count = this.count++): string {
         let result = "";
-        let length = this.chars.length;
+        let length = Mangler.chars.length;
 
         while (count >= 0) {
-            result = this.chars[count % length] + result;
+            result = Mangler.chars[count % length] + result;
 
             if (count > 0) {
                 count = Math.floor(count / length) - 1;
@@ -27,6 +41,7 @@ export class Mangler {
         return result;
     }
 
+    /** Transforms input string to a unique identifier, caching the result. */
     transform(from: string): string {
         let object = this.cache.get(from);
         if (object) {
@@ -34,7 +49,7 @@ export class Mangler {
         }
 
         this.cache.set(from, {
-            originName: from,
+            originalName: from,
             identifierName: this.createName(),
             referenceCount: 0,
         })
@@ -42,17 +57,26 @@ export class Mangler {
         return this.cache.get(from).identifierName;
     }
 
+    /** Converts input to CSS variable format if it exists in cache. */
     CSSVariableOf(value: string): string {
         return this.cache.get(value) ? `--${this.transform(value)}` : value;
     }
 
+    /**
+     * Prints the informations of all about this mangler.
+     * i.e. this is just printing for debugging.
+     */
     printLogs() {
         console.log(this.cache);
     }
 
+    /**
+     * Prints the informations of unreferenced unique identifier.
+     * i.e. this is just printing for warning.
+     */
     printLogsUnused() {
         for (const object of this.cache.values()) {
-            if (object.referenceCount == 0) console.log(`${object.originName} is not referenced.`);
+            if (object.referenceCount == 0) console.log(`${object.originalName} is not referenced.`);
         }
     }
 }
